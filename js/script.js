7fc645a2f6774378f839b3af42abf48b88cc7a34
verasans <veraifos@gmail.com>
2017-10-30 10:08:14 +0100

    fixed menu mobile
    now we can see the .active page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 ( function( $ ) {
 $( document ).ready(function() {
-$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+	var currentMenuItem = $('#cssmenu .active span').text();
+$('#cssmenu').prepend('<div id="menu-button">' + currentMenuItem + '</div>');
 	$('#cssmenu #menu-button').on('click', function(){
 		var menu = $(this).next('ul');
 		if (menu.hasClass('open')) {
